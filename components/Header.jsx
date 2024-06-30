6928e7b6023a529e607bb2170cbfe310b1559620
amnayounus <amnayounus999@gmail.com>
2024-06-30 05:20:17 +0500

    initial

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+import { Button } from './ui/button'
+
+const Header = () => {
+  return (
+    <header>Header</header>
+  )
+}
+
+export default Header
---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "15px",
+
+    },
+    screens: {
+      sm: "648px",
+      md: "768px",
+      lg: "960px",
+      xl: "1288px",
+    },
+    fontFamily: {
+      primary: "var(--font-jetbrainsMono)",
+    },
+    extend: {
+      colors: {
+        primary: '#1E1D2B', 
+        accent: {
+          DEFAULT: '#8E60F3',
+          hover: '#8841C5',
+        }
+
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
