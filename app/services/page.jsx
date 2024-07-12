4d76d85bbe0eaecbbc920f9b7f0ba2ecc26e93f9
amnayounus <amnayounus999@gmail.com>
2024-07-12 23:35:11 +0500

    servicesFormat Changed

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,30 +1,21 @@
 "use client";
 import React from 'react';
-import { BsArrowDownRight } from "react-icons/bs";
+import { BsArrowDownRight, BsGear } from "react-icons/bs";
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 
 const services = [
   {
-    num: '01',
     title: 'Web Development',
     description: 'Commodo amet dolore voluptate ea nostrud anim. Aliqua anim exercitation ea labore magna sint elit.',
     href: ''
   },
   {
-    num: '02',
     title: 'Web Development',
     description: 'Commodo amet dolore voluptate ea nostrud anim. Aliqua anim exercitation ea labore magna sint elit.',
     href: ''
   },
   {
-    num: '03',
-    title: 'Web Development',
-    description: 'Commodo amet dolore voluptate ea nostrud anim. Aliqua anim exercitation ea labore magna sint elit.',
-    href: ''
-  },
-  {
-    num: '04',
     title: 'Web Development',
     description: 'Commodo amet dolore voluptate ea nostrud anim. Aliqua anim exercitation ea labore magna sint elit.',
     href: ''
@@ -39,27 +30,27 @@ const Services = () => {
           opacity: 1,
           transition: { delay: 2.4, duration: 0.4, ease: "easeIn" }
         }}
-        className="grid grid-cols-1 md:grid-cols-2 gap-[60px]">
+        className="grid grid-cols-1 md:grid-cols-3 gap-[60px]">
           {services.map((service, index) => {
             return (
-              <div key={index} className="flex-1 flex flex-col justify-center gap-6 group">
-                <div className="w-full flex justify-between items-center">
-                  <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">{service.num}</div>
-                  <Link href={service.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45">
-                    <BsArrowDownRight className="text-primary text-3xl" />
+              <div 
+                key={index} 
+                className="flex-1 flex flex-col justify-center gap-6 group bg-[#27272c] p-6 rounded-lg transition-all duration-500 hover:bg-accent"
+              >
+                <div className="flex justify-center mb-4">
+                  <BsGear className="text-6xl text-white transition-all duration-500 group-hover:text-primary" />
+                </div>
+                <h2 className="text-[42px] font-bold leading-none text-white transition-all duration-500 text-center group-hover:text-primary">
+                  {service.title}
+                </h2>
+                <p className="text-white/60 group-hover:text-primary">{service.description}</p>
+                <div className="flex justify-end ">
+                  <Link href={service.href} className="w-[70px] h-[70px] rounded-full bg-white transition-all duration-500 flex justify-center items-center group-hover:bg-primary hover:-rotate-45">
+                    <BsArrowDownRight className="text-primary text-3xl group-hover:text-white " />
                   </Link>
                 </div>
-                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">{service.title}</h2>
-
-                <p className="text-white/60">{service.description}</p>
-
-                <div className="border-b border-white/20 w-full
-                "></div>
-
               </div>
-
             );
-
           })}
         </motion.div>
       </div>
